7d555520e5626eac506c33fb9bf181b6914e19c5
ng911datasync <dylan@olingeographics.com>
2020-06-08 20:03:29 -0600

    added coordinates widget

--- src/data/app.ts
+++ src/data/app.ts
@@ -7,7 +7,7 @@ import { getGWICPopup } from '../popup';
 import { gwMLayer, wTSLayer, notesLayer, walkDownLayers } from './layers';
 import esri = __esri;
 
-const version = '1.2.4';
+const version = '1.2.5';
 document.getElementById('version').innerText = 'v:' + version;
 
 export const display = '2D';
